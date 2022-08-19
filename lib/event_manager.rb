4149afbe991061562b20986e60fb78bb95a1045c
malcolmbly <malcolmhbailey@gmail.com>
2022-08-19 10:50:06 +0100

    Added ERB templating

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 puts 'Event Manager Initialized!'
 
 def clean_zipcode(zipcode)
@@ -15,24 +16,45 @@ def query_civic_api(zipcode)
   begin
     response = civic_api.representative_info_by_address(address: zipcode, levels: 'country', 
                                                         roles: ['legislatorUpperBody', 'legislatorLowerBody'])
-    response.officials.map(&:name).join(', ')
+    response.officials
   rescue
     'You can find representatives by googling it, duh.'
   end
 end
 
-file_name = 'event_attendees.csv'
-if File.exist?(file_name)
-  reader = CSV.open(
-    file_name,
-    headers: true,
-    header_converters: :symbol
-  )
+def load_erb_template
+  template_letter = File.read('form_letter.erb')
+  ERB.new template_letter
 end
 
-until reader.eof?
-  contents = reader.readline
-  contents[:zipcode] = clean_zipcode(contents[:zipcode])
-  representative = query_civic_api(contents[:zipcode])
-  p [contents[:first_name], contents[:zipcode], representative]
+def save_file(id)
+  form_letter = load_erb_template.result(binding)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
+
+def read_csv(file_name)
+  if File.exist?(file_name)
+    reader = CSV.open(
+      file_name,
+      headers: true,
+      header_converters: :symbol
+    )
+  end
+
+  until reader.eof?
+    contents = reader.readline
+    id = contents[0]
+    zipcode = clean_zipcode(contents[:zipcode])
+    first_name = contents[:first_name]
+    representatives = query_civic_api(contents[:zipcode])
+    save_file(id)
+  end
+end
+
+file_name = 'event_attendees.csv'
+read_csv(file_name)
